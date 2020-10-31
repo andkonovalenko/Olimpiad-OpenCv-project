4e8027df941c4b55b2f6eb4d9888e46eefa3838a
Alexandr <furgaylov5@gmail.com>
2020-10-31 16:29:52 +0200

    Fixed image with value None.

--- main.py
+++ main.py
@@ -1,28 +1,34 @@
 import cv2
-import imutils
 import numpy as np
 
 
 def save_video_result():
-    filename = "resources/sample-5.mp4"
-    output = "out.avi"
+    filename = str(input("Enter path video to process: "))
+    output = str(input("Enter path to result video: "))
 
     capture = cv2.VideoCapture(filename)
     fourcc = cv2.VideoWriter_fourcc('F', 'M', 'P', '4')
     video_recorder = cv2.VideoWriter(output, fourcc, 20.0, (int(capture.get(3)), int(capture.get(4))))
+    current_len_of_circles = 0
 
     while capture.isOpened():
         ret, frame = capture.read()
-        # frame = imutils.resize(frame, width=400)
+        if frame is None:
+            break
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
         gray = cv2.cvtColor(hsv, cv2.COLOR_BGR2GRAY)
         gray = cv2.medianBlur(gray, 5)
         circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, 20, param1=100, param2=40, minRadius=50, maxRadius=100)
+
         count = 0
         if circles is not None:
-            print(len(circles[0, :]))
+            len_of_circles = len(circles[0, :])
+            if len_of_circles > current_len_of_circles:
+                current_len_of_circles = len_of_circles
             circles = np.uint16(np.around(circles))
             for i in circles[0, :]:
+                print(i)
+                print(i)
                 count += 1
                 center = (i[0], i[1])
                 # circle center
@@ -37,12 +43,17 @@ def save_video_result():
                 color = (255, 0, 0)
 
                 cv2.putText(frame, str(count), center, font, font_scale, color, thickness, cv2.LINE_AA)
+
         video_recorder.write(frame)
         cv2.imshow('frame', frame)
+
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     capture.release()
     cv2.destroyAllWindows()
+
+def getTextInfo(fileName, count_or_circles):
+    pass
 
 
 # Press the green button in the gutter to run the script.
